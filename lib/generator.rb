280d1a44ed88f4aff157586b6a7bcaee982bdac0
Hrvoje Šimić <shime@twobucks.co>
2018-01-10 20:07:53 +0100

    feat: add generator

---
+++ lib/generator.rb
@@ -0,0 +1,22 @@
+class Generator
+  class UnrecognizedNodeError < StandardError; end
+
+  def call(node)
+    case node[:type]
+    when "Program"
+      node[:body].map {|body| call(body)}.join("\n")
+    when "ExpressionStatement"
+      call(node[:expression])
+    when "CallExpression"
+      call(node[:callee]) + "(" + node[:arguments].map {|argument| call(argument)}.join(",") + ")"
+    when "Identifier"
+      node[:name]
+    when "NumberLiteral"
+      node[:value]
+    when "StringLiteral"
+      '"' + node[:value] + "'"
+    else
+      raise UnrecognizedNodeError.new(node[:type])
+    end
+  end
+end
---
+++ test/generator_test.rb
@@ -0,0 +1,35 @@
+require "minitest/autorun"
+require "minitest/color"
+require_relative "../lib/generator"
+
+describe Generator do
+  before do
+    @generator = Generator.new
+    @input = {
+      type: "Program",
+      body: [{
+        type: "ExpressionStatement",
+        expression: {
+          type: "CallExpression",
+          callee: {
+            type: "Identifier",
+            name: "add"
+          },
+          arguments: [{
+            type: "NumberLiteral",
+            value: 1
+          },{
+            type: "NumberLiteral",
+            value: 2
+          }]
+        }
+      }]
+    }
+
+    @output = "add(1,2)"
+  end
+
+  it "generates #{@output} for #{@input}" do
+    @generator.call(@input).must_equal @output
+  end
+end
